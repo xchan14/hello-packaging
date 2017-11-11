bd228b1fb5983a379aa601f7139b9292204674db
Christian Camilon <christiancamilon@gmail.com>
2017-11-11 22:51:56 +0800

    packaging

---
+++ build/CMakeFiles/com.github.xchan14.gtk-hello-again.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "src/hello-again.c"
+  "com.github.xchan14.gtk-hello-again_valac.stamp"
+  "CMakeFiles/com.github.xchan14.gtk-hello-again.dir/src/hello-again.c.o"
+  "com.github.xchan14.gtk-hello-again.pdb"
+  "com.github.xchan14.gtk-hello-again"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/com.github.xchan14.gtk-hello-again.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
